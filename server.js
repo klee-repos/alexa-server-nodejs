c011605a2f3bffd19ee46e22b45d2c570f35f602
Kevin Lee <kevin.lee@lynkr.xyz>
2017-11-04 15:52:04 -0700

    fixes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var AlexaAppServer = require('alexa-app-server');
 var instance = AlexaAppServer.start({
   server_root: __dirname,     // Path to root
   //public_html: "public_html", // Static content
-  //app_dir: "apps",            // Location of alexa-app modules
+  app_dir: "apps",            // Location of alexa-app modules
   //app_root: "/alexa/",        // Service root
   httpsEnabled: false,
   port: process.env.PORT || 8080
