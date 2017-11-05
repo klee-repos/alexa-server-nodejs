996b8a05fd878a1080813ad594ca8150c651c3e9
Kevin Lee <kevin.lee@lynkr.xyz>
2017-11-04 17:07:09 -0700

    additions

--- apps/flip/index.js
+++ apps/flip/index.js
@@ -25,10 +25,26 @@ app.intent('FlipIntent',
 		"{FirstName} is gonna do a flip"
 	]
   },
-  function(request,response) {
+  function (request,response) {
     var name = request.slot('FirstName');
-    response.say(name + " wants to do a flip!");
+    response.say(name + " wants to do a flip!").shouldEndSession(false);
+    response.say("Would you like to hear a quote?");
   }
 );
+
+app.intent("QuoteIntent",
+	{
+		"utterances": [
+			"Tell me a quote",
+			"Yes",
+			"I would love to hear a quote",
+			"Yes, I would like to hear a quote",
+			"Yes, I would like to hear one"
+		]
+	},
+	function (request, response) {
+		response.say("Gavin looves to do flips!").shouldEndSession(true);
+	}
+)
 
 module.exports = app;
