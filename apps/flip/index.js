d42fd7d24e1ffcb5df6112c7f4548e3e66cb80b6
Kevin Lee <kevin.lee@lynkr.xyz>
2017-11-04 21:17:52 -0700

    mongodb atlas connection

--- apps/flip/index.js
+++ apps/flip/index.js
@@ -4,6 +4,20 @@
 var alexa = require('alexa-app');
 var app = new alexa.app('flip');
 
+// Connection to MongoDB Atlas
+var MongoClient = require("mongodb").MongoClient;
+var uri = "mongodb://lynkr:aviademy@test-shard-00-00-mlsxy.mongodb.net:27017,test-shard-00-01-mlsxy.mongodb.net:27017,test-shard-00-02-mlsxy.mongodb.net:27017/test?ssl=true&replicaSet=test-shard-0&authSource=admin";
+var database;
+
+MongoClient.connect(uri, function(err, db) {
+	if (err) {
+		console.log("MongoDB error: " + err);
+	} else {
+		database = db;
+		console.log("Sucessfully connected to MongoDB Atlas");
+	}
+})
+
 
 app.launch(function(request, response) {
 	response.say("Who wants to do a flip?").reprompt("Who wants to do a flip?").shouldEndSession(false);
@@ -16,6 +30,25 @@ app.error = function(exception, request, response) {
 	response.say( 'The following error has occurred. ' + error.message);
 };
 
+app.intent("GetRequestsIntent", 
+	{
+		"utterances": [
+			"Show request logs",
+			"Show me request logs",
+			"Request logs"
+		]
+	},
+	function (request, response) {
+		database.collection('alexa-requests').find().toArray(function(err,result) {
+			if (err) {
+				console.log(err);
+			} else {
+				console.log(result);
+			}
+		})
+	}
+)
+
 app.intent('FlipIntent',
   {
     "slots":{"FirstName":"AMAZON.Person"}
@@ -26,6 +59,13 @@ app.intent('FlipIntent',
 	]
   },
   function (request,response) {
+  	database.collection('alexa-requests').save(request, function(err, result) {
+  		if (err) {
+  			console.log("There was an error saving request to MongoDB Atlas");
+  		} else {
+  			console.log("Request saved to MongoDB Atlas");
+  		}
+  	});
     var name = request.slot('FirstName');
     response.say(name + " wants to do a flip!");
     response.say("Want to know a secret?").reprompt("Want to know a secret?").shouldEndSession(false);
@@ -45,6 +85,7 @@ app.intent("SecretIntent",
 	},
 	function (request, response) {
 		response.say("Gavin loves to do flips!").shouldEndSession(true);
+		database.close();
 	}
 )
 
